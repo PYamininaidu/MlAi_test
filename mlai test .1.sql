97ff5fa98558acd3e69aa12f770f80bd28783217
PYamininaidu <113752054+PYamininaidu@users.noreply.github.com>
2023-10-25 15:48:11 +0530

    Add files via upload

---
+++ mlai test .1.sql
@@ -0,0 +1,29 @@
+-- 1. Are there any holidays in March?
+SELECT Holiday
+FROM table_holidaylist
+WHERE MONTH(Date) = 3;
+
+-- 2. Sorry, I meant December
+SELECT Holiday
+FROM table_holidaylist
+WHERE MONTH(Date) = 12;
+
+-- 3. When does this year's holiday marking the end of Ramadan start?
+SELECT Date
+FROM table_holidaylist
+WHERE Holiday = 'Eid al-Fitr'
+ORDER BY Date
+LIMIT 1;
+
+-- 4. How many days is it celebrated for this year?
+SELECT DATEDIFF(
+    (SELECT Date FROM table_holidaylist WHERE Holiday = 'Eid al-Fitr'),
+    (SELECT Date FROM table_holidaylist WHERE Holiday = 'Eid al-Fitr Holiday')
+) + 1;
+
+-- 5. Today is July 16. When is the nearest holiday?
+SELECT Holiday, Date
+FROM table_holidaylist
+WHERE Date > '2023-07-16'
+ORDER BY Date
+LIMIT 1;
