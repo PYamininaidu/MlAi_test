365bd44d3b77b242a1980cbc0f0b3fe7807e10a9
PYamininaidu <113752054+PYamininaidu@users.noreply.github.com>
2023-10-25 15:59:21 +0530

    Update mlai test .1.sql

--- mlai test .1.sql
+++ mlai test .1.sql
@@ -11,14 +11,14 @@ WHERE MONTH(Date) = 12;
 -- 3. When does this year's holiday marking the end of Ramadan start?
 SELECT Date
 FROM table_holidaylist
-WHERE Holiday = 'Eid al-Fitr'
+WHERE Holiday = 'Eid al-Adha'
 ORDER BY Date
 LIMIT 1;
 
 -- 4. How many days is it celebrated for this year?
 SELECT DATEDIFF(
-    (SELECT Date FROM table_holidaylist WHERE Holiday = 'Eid al-Fitr'),
-    (SELECT Date FROM table_holidaylist WHERE Holiday = 'Eid al-Fitr Holiday')
+    (SELECT Date FROM table_holidaylist WHERE Holiday = 'Eid al-Adha'),
+    (SELECT Date FROM table_holidaylist WHERE Holiday = 'Eid al-Adha Holiday')
 ) + 1;
 
 -- 5. Today is July 16. When is the nearest holiday?
